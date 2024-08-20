ffcba6243013e7af818f52237ef2632e723d1c1e
Sobuz <sobuz.info0298@gmail.com>
2024-08-20 23:20:03 +0600

    create apperror class

--- src/app/modules/Auth/auth.services.ts
+++ src/app/modules/Auth/auth.services.ts
@@ -2,6 +2,10 @@ import { TUser } from "../User/user.interface";
 import { User } from "../User/user.model";
 
 const createUserIntoDB = async (payload: TUser) => {
+  const isUserExist = await User.findOne({ email: payload.email });
+  if (isUserExist) {
+    throw new Error("This user already exist");
+  }
   const result = await User.create(payload);
   return result;
 };
--- src/app/modules/User/user.model.ts
+++ src/app/modules/User/user.model.ts
@@ -1,5 +1,8 @@
+/* eslint-disable @typescript-eslint/no-this-alias */
 import { model, Schema } from "mongoose";
 import { TUser } from "./user.interface";
+import bcrypt from "bcrypt";
+import config from "../../config";
 
 const userSchema = new Schema<TUser>(
   {
@@ -34,4 +37,20 @@ const userSchema = new Schema<TUser>(
   }
 );
 
-export const User = model<TUser>("User", userSchema);
+// password hashed using pre hooks
+
+userSchema.pre("save", async function (next) {
+  const user = this;
+  user.password = await bcrypt.hash(
+    user.password,
+    Number(config.bcrypt_salt_rounds)
+  );
+  next();
+});
+// set "" after saving password
+userSchema.post("save", function (doc, next) {
+  doc.password = "";
+  next();
+});
+
+export const User = model<TUser>("user", userSchema);
